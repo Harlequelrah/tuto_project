8493d3051da0b6fc377adae7398118eb481504cf
Harlequelrah <degbovimax19@gmail.com>
2025-06-04 17:40:15 +0000

    learning about ListTitle , Icon , Scaffold , AppBar , MaterialApp , Wrapp

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.tealAccent,
           brightness: Brightness.dark,
         ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(25.0),
-        height: 300,
-        child: Stack(
-          children: [
-            Image.asset('assets/images/bg.jpg', fit: BoxFit.cover),
-            SizedBox(
-              child: Center(
-                child: Text(
-                  "My sweety cold life",
-                  style: TextStyle(color: Colors.black87),
-                ),
-              ),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Harlequelrah App"),
+          centerTitle: true,
+          leading: Icon(Icons.login),
+          actions: [Icon(Icons.logout)],
+          backgroundColor: Colors.teal,
         ),
       ),
     );
