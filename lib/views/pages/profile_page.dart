180e50fa38ca09400d4c5fd9b93caabcd11e0a76
Harlequelrah <degbovimax19@gmail.com>
2025-06-08 19:31:17 +0000

    learning about widgets : Switch , SwitchListTile , SingleChildScrollView , GestureDetector , InkWell and Slider

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -10,40 +10,84 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = false;
+  bool isSwitched = false;
+  double sliderValue = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Checkbox(
-            value: isChecked,
-            tristate: true,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile(
-            title: Text("Click me please"),
-            value: isChecked,
-            tristate: true,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          Text(controller.text),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+            Checkbox(
+              value: isChecked,
+              tristate: true,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: Text("Click me please"),
+              value: isChecked,
+              tristate: true,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch(
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text("Switch me"),
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider(
+              max: 100.0,
+              divisions: 10,
+              value: sliderValue,
+              onChanged: (double value) {
+                setState(() {
+                  sliderValue = value;
+                  print(value);
+                });
+              },
+            ),
+            InkWell(
+              splashColor: Colors.amber,
+              onTap: () {
+                print("Image clicked");
+              },
+              // child: Image.asset("assets/images/bg.jpg"),
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.white12,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
